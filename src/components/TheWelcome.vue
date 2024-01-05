94c7c12fcdc027103d27b7c93c1337c477f46483
Dawid <dawid3104@onet.pl>
2024-01-05 01:49:35 +0100

    Fixes init

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -15,7 +15,10 @@ import SupportIcon from "./icons/IconSupport.vue";
     <template #heading>Documentation</template>
 
     Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener"
+    <a
+      href="https://vuejs.org/"
+      target="_blank"
+      rel="noopener"
       >official documentation</a
     >
     provides you with all information you need to get started.
@@ -34,7 +37,10 @@ import SupportIcon from "./icons/IconSupport.vue";
       rel="noopener"
       >Vite</a
     >. The recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener"
+    <a
+      href="https://code.visualstudio.com/"
+      target="_blank"
+      rel="noopener"
       >VSCode</a
     >
     +
@@ -44,9 +50,17 @@ import SupportIcon from "./icons/IconSupport.vue";
       rel="noopener"
       >Volar</a
     >. If you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a>
+    <a
+      href="https://www.cypress.io/"
+      target="_blank"
+      rel="noopener"
+      >Cypress</a
+    >
     and
-    <a href="https://on.cypress.io/component" target="_blank" rel="noopener"
+    <a
+      href="https://on.cypress.io/component"
+      target="_blank"
+      rel="noopener"
       >Cypress Component Testing</a
     >.
 
@@ -62,14 +76,28 @@ import SupportIcon from "./icons/IconSupport.vue";
     <template #heading>Ecosystem</template>
 
     Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener"
+    <a
+      href="https://pinia.vuejs.org/"
+      target="_blank"
+      rel="noopener"
+      >Pinia</a
+    >,
+    <a
+      href="https://router.vuejs.org/"
+      target="_blank"
+      rel="noopener"
       >Vue Router</a
     >,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener"
+    <a
+      href="https://test-utils.vuejs.org/"
+      target="_blank"
+      rel="noopener"
       >Vue Test Utils</a
     >, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener"
+    <a
+      href="https://github.com/vuejs/devtools"
+      target="_blank"
+      rel="noopener"
       >Vue Dev Tools</a
     >. If you need more resources, we suggest paying
     <a
@@ -88,7 +116,11 @@ import SupportIcon from "./icons/IconSupport.vue";
     <template #heading>Community</template>
 
     Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a
+    <a
+      href="https://chat.vuejs.org"
+      target="_blank"
+      rel="noopener"
+      >Vue Land</a
     >, our official Discord server, or
     <a
       href="https://stackoverflow.com/questions/tagged/vue.js"
@@ -96,11 +128,17 @@ import SupportIcon from "./icons/IconSupport.vue";
       rel="noopener"
       >StackOverflow</a
     >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener"
+    <a
+      href="https://news.vuejs.org"
+      target="_blank"
+      rel="noopener"
       >our mailing list</a
     >
     and follow the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+    <a
+      href="https://twitter.com/vuejs"
+      target="_blank"
+      rel="noopener"
       >@vuejs</a
     >
     twitter account for latest news in the Vue world.
@@ -114,7 +152,10 @@ import SupportIcon from "./icons/IconSupport.vue";
 
     As an independent project, Vue relies on community backing for its
     sustainability. You can help us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener"
+    <a
+      href="https://vuejs.org/sponsor/"
+      target="_blank"
+      rel="noopener"
       >becoming a sponsor</a
     >.
   </WelcomeItem>
